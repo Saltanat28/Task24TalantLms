09cf83ff832a82a162280010c9bea50c7fbe2201
saltanat <saltanatnematillakyzy6@gmail.com>
2022-12-07 17:21:14 +0600

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Set<Integer> set1 = new HashSet<>();
+        set1.add(4);
+        set1.add(3);
+        set1.add(1);
+        System.out.println(set1);
+
+        Set<Integer> set2 = new HashSet<>();
+        set2.add(3);
+        set2.add(1);
+        set2.add(9);
+        System.out.println(set2);
+        System.out.println();
+        System.out.println();
+        System.out.println(symmetricDifference(set1, set2));
+
     }
+
+        public static Set<Integer> symmetricDifference(Set<Integer> set1, Set<Integer> set2){
+            Set<Integer> set3 = new TreeSet<>(set1);
+            set3.addAll(set2);
+            Set<Integer> sort = new HashSet<>(set1);
+            sort.retainAll(set2);
+            set3.removeAll(sort);
+            return set3;
+        }
+
+
 }
